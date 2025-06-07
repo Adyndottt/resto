283e0a51f29458b0b1241411adcad16469481e59
Adyndottt <adyndott@students.unnes.ac.id>
2025-06-08 00:48:20 +0700

    Update 3_Prediksi.py

--- pages/3_Prediksi.py
+++ pages/3_Prediksi.py
@@ -2,19 +2,19 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-st.title("🔮 Prediksi Kategori Restoran")
+st.title("🔮 Prediksi Nilai Resto Rating")
 
 # Load model
 try:
     model = joblib.load("model/trained_model.pkl")
 except:
-    st.error("Model belum dilatih. Silakan latih di halaman Model.")
+    st.error("Model belum tersedia. Silakan latih model terlebih dahulu.")
     st.stop()
 
-# Ambil fitur yang digunakan model
+# Ambil nama fitur dari model
 feature_names = model.feature_names_in_
 
-st.subheader("Isi Formulir")
+st.subheader("Isi Formulir Data Restoran")
 user_input = {}
 for feature in feature_names:
     user_input[feature] = st.number_input(f"{feature}", step=1.0)
@@ -23,4 +23,4 @@ for feature in feature_names:
 if st.button("Prediksi"):
     input_df = pd.DataFrame([user_input])
     prediction = model.predict(input_df)[0]
-    st.success(f"Kategori restoran diprediksi sebagai: **{prediction}**")
+    st.success(f"Prediksi rating restoran: **{prediction:.2f}**")
